9a260bdcd4eee046bf3abe11ff1639dbb641817b
Devontae <118658527+s2kshare@users.noreply.github.com>
2024-06-07 13:52:38 +1200

    SURELY THIS WORKS

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/dev-portfolio/",
+    base: "/demo-portfolio/",
+    predeploy: "npm run build",
+    deploy: "gh-pages -d dist",
 });
